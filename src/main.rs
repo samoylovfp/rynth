692b54596c9ccd653372b05b9b7b12a956f9a9cb
Filipp Samoilov <samoylovfp@gmail.com>
2024-06-23 00:16:23 +0300

    Read MIDI

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
-use std::{fs::File, process::exit, sync::Arc, time::Duration};
+const USAGE: &str = "Pass sf2 file as the first arg and midi device name as the second one";
 
-use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+use std::{
+    fs::File,
+    process::exit,
+    sync::{mpsc::channel, Arc},
+    time::Duration,
+};
+
+use cpal::{traits::{DeviceTrait, HostTrait, StreamTrait}, BufferSize, StreamConfig};
+use midi_msg::{ChannelVoiceMsg, MidiMsg};
+use midir::MidiInput;
 use rustysynth::{SoundFont, Synthesizer, SynthesizerSettings};
 
 fn main() {
-    let usage = "Pass sf2 file as the first arg";
     let sf_file = std::env::args().nth(1).unwrap_or_else(|| {
-        println!("{usage}");
+        println!("{USAGE}");
         exit(1);
     });
 
@@ -18,19 +26,65 @@ fn main() {
     let settings = SynthesizerSettings::new(44100);
     let mut synthesizer = Synthesizer::new(&sound_font, &settings).unwrap();
 
-    // Play some notes (middle C, E, G).
-    synthesizer.note_on(0, 60, 100);
-    synthesizer.note_on(0, 64, 100);
-    synthesizer.note_on(0, 67, 100);
+    let (midi_sender, midi_receiver) = channel();
+
+    // MIDI
+    let midi_in = MidiInput::new("riano").unwrap();
+    let in_ports = midi_in.ports();
+    let device_to_search = std::env::args().nth(2);
+
+    println!("Discovered MIDI devices:");
+    for p in &in_ports {
+        println!("{:?}", midi_in.port_name(p))
+    }
+
+    let midi_device = match device_to_search {
+        None => {
+            println!("{USAGE}");
+            exit(1);
+        }
+        Some(arg_p) => in_ports
+            .iter()
+            .find(|p| midi_in.port_name(p).unwrap().contains(&arg_p))
+            .unwrap_or_else(|| panic!("MIDI device {arg_p:?} not found")),
+    };
+
+    println!("Connecting to {}", midi_in.port_name(midi_device).unwrap());
+
+    let _conn = midi_in
+        .connect(
+            midi_device,
+            "reading midi",
+            move |_t, m, _| {
+                let (m, _len) = MidiMsg::from_midi(m).unwrap();
+                if let MidiMsg::ChannelVoice { msg, .. } = m {
+                    midi_sender.send(msg).unwrap();
+                }
+            },
+            (),
+        )
+        .unwrap();
 
     // Audio stream
     let host = cpal::default_host();
     let dev = host.default_output_device().unwrap();
-    let conf = dev.default_output_config().unwrap();
+    let mut conf: StreamConfig = dev.default_output_config().unwrap().into();
+    conf.buffer_size = BufferSize::Fixed(512);
     let stream = dev
         .build_output_stream(
-            &conf.into(),
+            &conf,
             move |d: &mut [f32], _| {
+                while let Ok(msg) = midi_receiver.try_recv() {
+                    match msg {
+                        ChannelVoiceMsg::NoteOn { note, velocity } => {
+                            synthesizer.note_on(0, note as i32, velocity as i32);
+                        }
+                        ChannelVoiceMsg::NoteOff { note, .. } => {
+                            synthesizer.note_off(0, note as i32);
+                        }
+                        _ => {}
+                    }
+                }
                 let mut left = vec![0.0; d.len() / 2];
                 let mut right = vec![0.0; d.len() / 2];
                 synthesizer.render(&mut left, &mut right);
